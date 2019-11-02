c893af2652ac6d895082445283d9d74756e6c631
dexxxed <dexxxed.app@gmail.com>
2019-11-02 17:31:04 +0200

    Добавьте файлы проекта.

---
+++ textBox5/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace textBox5
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox2 = new System.Windows.Forms.RichTextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(15, 9);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(383, 122);
+			this.textBox2.TabIndex = 0;
+			this.textBox2.Text = "";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(312, 146);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(86, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Сохранить";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(23, 151);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(67, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Имя файла:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(96, 148);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(210, 20);
+			this.textBox3.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.ClientSize = new System.Drawing.Size(414, 194);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox2);
+			this.Name = "Form1";
+			this.Text = "Файловый менеджер";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RichTextBox textBox2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox3;
+	}
+}
+
---
+++ textBox5/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace textBox5
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (textBox3.Text != "" && textBox2.Text != "")
+			{
+				StreamWriter write_text;
+				FileInfo file = new FileInfo(textBox3.Text);
+				write_text = file.AppendText();
+				write_text.WriteLine(textBox2.Text);
+				write_text.Close();
+				MessageBox.Show("Ваш текст записан в файл " + textBox3.Text, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			else
+			{
+				MessageBox.Show("Заполните два текстовых окна!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+
+		}
+
+	}
+}
